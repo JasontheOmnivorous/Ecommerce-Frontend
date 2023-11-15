a2506547f65a9d47d9e95324ee27090be15a7064
HtetKoKoLwin <htetkokolwin189@gmail.com>
2023-11-15 20:45:59 +0630

    initial

--- src/form/SignUp.tsx
+++ src/form/SignUp.tsx
@@ -1,14 +1,54 @@
+import GoogleIcon from "@mui/icons-material/Google";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Box } from "@mui/material";
 const Signup = () => {
   return (
     <>
-      <form className="form">
-        <label>
-          FirstName <input type="text" id="firstname"></input>
-        </label>
-        <label>
-          LastName <input type="text" id="lastname"></input>
-        </label>
-      </form>
+      <Box>
+        <div className="formContainer">
+          <form className="form">
+            <div className="inputcontainer">
+              <label>FirstName : </label>
+              <input type="text" id="firstname" />
+            </div>
+            <div className="inputcontainer">
+              <label>LastName : </label>
+              <input type="text" id="lastname" />
+            </div>
+            <div className="inputcontainer">
+              <label>Password : </label>
+              <input type="password" id="password" />
+            </div>
+            <div className="inputcontainer">
+              <label>Email : </label>
+              <input type="email" id="email" />
+            </div>
+
+            <label>
+              <input type="radio" /> Male
+              <input type="radio" /> Female
+              <input type="radio" /> Other
+            </label>
+            <label>
+              <input type="checkbox"></input> Forgot Password
+            </label>
+            <div className="brakeLine"></div>
+            <div className="google">
+              <GoogleIcon />
+              <p>Login With Google</p>
+            </div>
+            <div className="twitter">
+              <TwitterIcon />
+              <p>Login With Twitter</p>
+            </div>
+            <div className="linkIn">
+              <LinkedInIcon />
+              <p>Login With LinkedIn</p>
+            </div>
+          </form>
+        </div>
+      </Box>
     </>
   );
 };
