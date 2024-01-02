a21d88c8fe7815ae8a8d1b65891d4e00d26c174a
JasontheOmnivorous <marsjason485@gmail.com>
2024-01-02 20:53:26 +0630

    refactor routes

--- src/approute/AppRouter.tsx
+++ src/approute/AppRouter.tsx
@@ -1,3 +1,4 @@
+import { ReactNode } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "../App";
 import Layout from "../components/layout/Layout";
@@ -6,7 +7,33 @@ import CartPage from "../pages/cart/CartPage";
 import ItemListPage from "../pages/item-list/ItemListPage";
 import Item from "./../pages/item-list/Item";
 
+interface Props {
+  id: number;
+  path: string;
+  component: ReactNode;
+}
+
 const AppRouter = () => {
+  const routes = [
+    { id: 1, path: "/item-list", component: <ItemListPage /> },
+    { id: 2, path: "/item-list/:id", component: <Item /> },
+    { id: 3, path: "/cart", component: <CartPage /> },
+  ];
+
+  const generateRoute = ({ id, path, component }: Props) => {
+    return (
+      <Route
+        key={id}
+        path={path}
+        element={
+          <AuthGuard>
+            <Layout>{component}</Layout>
+          </AuthGuard>
+        }
+      />
+    );
+  };
+
   return (
     <BrowserRouter>
       <Routes>
@@ -18,36 +45,7 @@ const AppRouter = () => {
             </Layout>
           }
         />
-        <Route
-          path="/item-list"
-          element={
-            <AuthGuard>
-              <Layout>
-                <ItemListPage />
-              </Layout>
-            </AuthGuard>
-          }
-        />
-        <Route
-          path="/item-list/:id"
-          element={
-            <AuthGuard>
-              <Layout>
-                <Item />
-              </Layout>
-            </AuthGuard>
-          }
-        />
-        <Route
-          path="/cart"
-          element={
-            <AuthGuard>
-              <Layout>
-                <CartPage />
-              </Layout>
-            </AuthGuard>
-          }
-        />
+        {routes.map((item) => generateRoute(item))}
       </Routes>
     </BrowserRouter>
   );
